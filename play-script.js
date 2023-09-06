1c2e487c30e01d8c5c84a6248153509b522f2d49
Nilanjana Bhattacharya <93534905+nila-2003@users.noreply.github.com>
2023-09-06 23:59:09 +0530

    Update play-script.js

--- play-script.js
+++ play-script.js
@@ -2,33 +2,26 @@ const gameBoard = document.getElementById("game-board");
 const scoreDisplay = document.getElementById("score");
 
 let score = 0;
-
-// Define game objects and their corresponding environmental messages
 const gameObjects = [
     { name: "Recycle", message: "Recycling helps save the environment!" },
     { name: "Plant Trees", message: "Planting trees reduces carbon emissions." },
     { name: "Reduce Water Usage", message: "Conserving water is essential for our planet." },
-    // Add more game objects and messages as needed
 ];
-
-// Function to create a game object element
 function createGameObject(object) {
     const gameObject = document.createElement("div");
     gameObject.classList.add("game-object");
     gameObject.innerText = object.name;
 
-    // Attach a click event listener to display the environmental message
     gameObject.addEventListener("click", () => {
         alert(object.message);
-        gameObject.style.display = "none"; // Remove the object after clicking
-        score += 10; // Increase the score
+        gameObject.style.display = "none"; 
+        score += 10; 
         scoreDisplay.innerText = `Score: ${score}`;
     });
 
     return gameObject;
 }
 
-// Function to populate the game board with objects
 function populateGameBoard() {
     gameBoard.innerHTML = "";
     for (const object of gameObjects) {
@@ -36,6 +29,4 @@ function populateGameBoard() {
         gameBoard.appendChild(gameObject);
     }
 }
-
-// Start the game by populating the game board
 populateGameBoard();
